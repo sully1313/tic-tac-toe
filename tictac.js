1ac25c17fcff4f6b84043463ba68cda9ca7805d0
Matt Sullivan <mattbsullivan@gmail.com>
2016-05-16 08:19:59 -0700

    Issues addressed Tictac Complete

--- tictac.js
+++ tictac.js
@@ -15,19 +15,73 @@ $(document).ready(function(){
 
     function checkForWin(player){
 //top row
-        if ($('#board').find('#1').text() !== ''){
-            if ($('#board').find('#1').text() == $('#board').find('#2').text()) {
-                if ($('#board').find('#1').text() == $('#3').text()) {
+        if($('#board').find('#1').text() !== ''){
+            if($('#board').find('#1').text() == $('#board').find('#2').text()) {
+                if($('#board').find('#1').text() == $('#3').text()) {
                     alert('Game over! '+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }
 //left column
-            if ($('#board').find('#1').text() == $('#board').find('#4').text()) {
-                if ($('#board').find('#1').text() == $('#7').text()) {
+            if($('#board').find('#1').text() == $('#board').find('#4').text()) {
+                if($('#board').find('#1').text() == $('#7').text()) {
                     alert('Game over! '+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }
+//left diagonal
+          if($('#board').find('#1').text() == $('#board').find('#5').text()){
+            if($('#board').find('#1').text() == $('#9').text()){
+              alert('Game over! '+player+' is the winner!');
+              $('#newGame').removeClass('invisible');
+            }
           }
         }
+//middle column
+          if($('#board').find('#2').text() !== ''){
+            if($('#board').find('#2').text() == $('#board').find('#5').text()){
+              if($('#board').find('#2').text() == $('#8').text()){
+                alert('Game over! '+player+' is the winner!');
+                $('#newGame').removeClass('invisible');
+              }
+            }
+          }
+//right column
+          if($('#board').find('#3').text() !== ''){
+            if($('#board').find('#3').text() == $('#board').find('#6').text()){
+              if($('#board').find('#3').text() == $('#9').text()){
+                alert('Game Over '+player+' is the winner!');
+                $('#newGame').removeClass('invisible');
+              }
+            }
+//right diagonal
+            if($('#board').find('#3').text() == $('#board').find('#5').text()){
+              if($('#board').find('#3').text() == $('#7').text()){
+                alert('Game Over '+player+' is the winner!');
+                $('#newGame').removeClass('invisible');
+              }
+            }
+          }
+//middle row
+          if($('#board').find('#4').text() !== ''){
+            if($('#board').find('#4').text() == $('#board').find('#5').text()){
+              if($('#board').find('#4').text() == $('#6').text()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+//bottom row
+        if ($('#board').find('#7').text() !== ''){
+            if ($('#board').find('#7').text() == $('#board').find('#8').text()) {
+                if ($('#board').find('#7').text() == $('#9').text()) {
+                    alert('Game over! '+player+' is the winner!');
+                    $('#newGame').removeClass('invisible');
+                }
+            }
+        }
+    }
+    $('#newGame').on('click',function(){
+        $('.square').text('');
+    });
+});
